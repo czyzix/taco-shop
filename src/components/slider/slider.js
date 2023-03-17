f1fee4d1a0e4823847df530ecfc035fea4177bcc
czyzix <czyzevsky@gmail.com>
2023-03-17 14:24:21 +0100

    Updated slider component

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -1,47 +1,39 @@
 import { useState } from "react";
 import "./slider.css";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 
 const Slider = (product) => {
 
-    /* const [images, setImages] = useState([
-        product.product.img_first,
-        product.product.img_second ? product.product.img_second : product.product.img_first,
-        product.product.img_third ? product.product.img_third : product.product.img_second,
-    ]);
+    const productImgs = product.product.imgs
+    const productImgsLenght = Object.keys(productImgs).length;
+    const [currentImg, setCurrentImg] = useState(0);
+    const [direction, setDirection] = useState("");
 
-    const [currentIndex, setCurrentIndex] = useState(0);
+    const prevSlide = () => {
+        setCurrentImg(currentImg === 0 ? productImgsLenght - 1 : currentImg - 1)
+        setDirection("left");
+    }
 
-    const nextImg = () => {
-        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-    };
-
-    const prevImg = () => {
-        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-    };
-    
-    return (
-        <div className="slider-container">
-            <button onClick={prevImg}>prev</button>
-            <img 
-                className="slider-img"
-                src={`../productsphotos/${images[currentIndex]}.jpg`} 
-                alt="img"
-            />
-            <button onClick={nextImg}>next</button>
-        </div>
-    ); */
-
-    const productImg = product.product.imgs
-
-    console.log(productImg);
+    const nextSlide = () => {
+        setCurrentImg(currentImg === productImgsLenght - 1 ? 0 : currentImg + 1)
+        setDirection("right");
+    }
 
     return (
         <div className="container-slider">
-            {Object.keys(productImg).map((key) => (
-                <img className="slider-img" src={`../productsphotos/${productImg[key]}.jpg`}  alt={key} />
+            {currentImg > 0 && <FiChevronLeft onClick={prevSlide} className="prev-btn"/>}
+            {Object.keys(productImgs).map((photo, index) => (
+                <div 
+                    className={index === currentImg ? `slide-active ${direction}` : "slide"}  
+                    key={index}
+                >
+                    {index === currentImg && (<img className="slider-img" src={`../productsphotos/${productImgs[photo]}.jpg`}  alt={photo} />)} 
+                </div>
             ))}
+            {currentImg < productImgsLenght - 1 && <FiChevronRight onClick={nextSlide} className="next-btn"/>}
         </div>
     )
 }
  
 export default Slider;
+
