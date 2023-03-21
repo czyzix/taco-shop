6a018ac349bb33fe2d7af4c6af2d751813ff577d
czyzix <czyzevsky@gmail.com>
2023-03-21 13:05:18 +0100

    Changed display of products to list

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -6,6 +6,8 @@ import Filters from "../filters/filters.js"
 const Shop = (props) => {
     const {products} = props;
 
+    
+
     const [search, setSearch] = useState('')
     console.log(search);
 
@@ -13,27 +15,53 @@ const Shop = (props) => {
         setSearch(e.target.value)
     };
 
+    /* const [selectedSortOption, setSelectedSortOption] = useState('A-Z');
+    const [visibleProducts, setVisibleProducts] = useState(products);
+
+    function handleSortChange(event) {
+        setSelectedSortOption(event.target.value);
+        sortProducts(event.target.value);
+    }
+
+    function sortProducts(sortOption) {
+        const sortedProducts = [...products];
+        if (sortOption === 'a-z') {
+          sortedProducts.sort((a, b) => a.name.localeCompare(b.name));
+        } else if (sortOption === 'z-a') {
+          sortedProducts.sort((a, b) => b.name.localeCompare(a.name));
+        } else if (sortOption === 'lowest-price') {
+          sortedProducts.sort((a, b) => a.price - b.price);
+        } else if (sortOption === 'highest-price') {
+          sortedProducts.sort((a, b) => b.price - a.price);
+        }
+        setVisibleProducts(sortedProducts);
+    } */
+
     return (
         <div>
             <h2>SHOP</h2>
             <Filters
                 handleInputChange={handleInputChange}
+                /* handleSortChange={handleSortChange}
+                selectedSortOption={selectedSortOption} */
             >
             </Filters>
-            <div className="products-container">
+            <ul className="products-container">
                 {products
                     .filter((product) => {
                         return search.toLowerCase().trim() === ''
-                        ? product : product.name.toLowerCase().includes(search);
+                            ? product : product.name.toLowerCase().includes(search);
                     })
                     .map((product) => (
-                        <Product
-                            key={product.id}
-                            product={product}
-                        >
-                        </Product>
+                        <li>
+                            <Product
+                                key={product.id}
+                                product={product}
+                            >
+                            </Product>
+                        </li>
                     ))}
-            </div>
+            </ul>
         </div>
     );
 }
