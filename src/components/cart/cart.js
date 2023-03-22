e0bc2c76834eee2a3e4e98d29d054a1a4dc8ef93
czyzix <czyzevsky@gmail.com>
2023-03-22 13:11:30 +0100

    Updated cart component content

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -1,6 +1,32 @@
-const Cart = () => {
+import "./cart.css";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+
+const Cart = (props) => {
+
+    const { cartItems, onAdd, onRemove } = props;
+
     return (
-        "cart"
+        <div>
+            <h2>CART</h2>
+            <div>
+                {cartItems.length === 0 && <p className="cart-is-empty">Cart is empty...</p>}
+                <div className="cart-items-container">
+                    {cartItems.map((item) => (
+                        <div key={item.id} className="cart-item">
+                            <img className="cart-item-img" src={`../productsphotos/${item.imgs.img_first}.jpg`} alt={item.name} />
+                            <p className="item-name">{item.name}</p>
+                            <div className="item-qty-section">
+                                <FiChevronLeft onClick={() => onRemove(item)} className="qty-btn" />
+                                <p>{item.qty}</p>
+                                <FiChevronRight onClick={() => onAdd(item)} className="qty-btn" />
+                            </div>
+                            <p className="item-price">{item.price.toFixed(2)}zł x {item.qty} = {(item.qty * item.price).toFixed(2)} zł</p>
+                        </div>
+                    ))}
+                </div>
+                
+            </div>
+        </div>
     )
 }
 
