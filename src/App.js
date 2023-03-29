a9433e02bcab8f79d130d399a9c2c6a5c31a57a7
czyzix <czyzevsky@gmail.com>
2023-03-29 10:50:16 +0200

    Added HashRoute

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Cart from './components/cart/cart.js';
 import Shop from './components/shop/shop.js';
 import Navbar from './components/navbar/navbar.js';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import data from './data.js';
 import { useState, useEffect, startTransition } from 'react';
 import Footer from './components/footer/footer.js';
